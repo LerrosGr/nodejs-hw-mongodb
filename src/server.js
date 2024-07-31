b4af03bc5eb6e3abcd316e3cce53d74167f59fd1
Lerros <108396158+LerrosGr@users.noreply.github.com>
2024-07-31 20:16:17 +0300

    finished task

---
+++ src/server.js
@@ -0,0 +1,72 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import dotenv from 'dotenv';
+
+import { getAllContacts, getContactById } from './services/contacts.js';
+
+dotenv.config();
+
+const PORT = Number(process.env.PORT) || 3000;
+
+export function setupServer() {
+  const app = express();
+
+  app.use(
+    pino({
+      transport: { target: 'pino-pretty' },
+    }),
+  );
+  app.use(cors());
+
+  app.get('/contacts', async (req, res) => {
+    try {
+      const contacts = await getAllContacts();
+
+      res.status(200).json({
+        status: 200,
+        message: 'Successfully found contacts!',
+        data: contacts,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json('Internal Server Error');
+    }
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    try {
+      const { contactId } = req.params;
+      const contact = await getContactById(contactId);
+
+      if (!contact) {
+        return res.status(404).json({
+          message: 'Contact not found',
+        });
+      }
+      res.status(200).json({
+        status: 200,
+        message: `Successfully found contact with id ${contactId}!`,
+        data: contact,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json('Internal Server Error');
+    }
+  });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({ message: 'Not found' });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+  });
+}
