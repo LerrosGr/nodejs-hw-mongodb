11a4e5a67e5b228284cd644ac54521a2f46697d6
Lerros <108396158+LerrosGr@users.noreply.github.com>
2025-10-06 16:06:11 +0300

    added request methods and code organization

--- src/server.js
+++ src/server.js
@@ -4,7 +4,10 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import { getEnvVar } from './utils/getEnvVar.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+
+import contactsRouter from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 dotenv.config();
 
@@ -21,51 +24,11 @@ export const setupServer = () => {
     }),
   );
 
-  app.get('/contacts', async (req, res) => {
-    try {
-      const contacts = await getAllContacts();
-      res.status(200).json({
-        status: 200,
-        message: 'Successfully found contacts!',
-        data: contacts,
-      });
-    } catch (e) {
-      console.error(e);
-      res.status(500).json('Internal Server Error');
-    }
-  });
+  app.use(contactsRouter);
 
-  app.get('/contacts/:contactId', async (req, res) => {
-    try {
-      const { contactId } = req.params;
-      const contact = await getContactById(contactId);
+  app.use(notFoundHandler);
 
-      if (!contact) {
-        return res.status(404).json({ message: 'Contact not foound' });
-      }
-      res.status(200).json({
-        status: 200,
-        message: 'Successfully found contact with id {contactId}!',
-        data: contact,
-      });
-    } catch (e) {
-      console.error(e);
-      res.status(500).json('Internal Server Error');
-    }
-  });
-
-  app.use((req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,3 +9,50 @@ export const getContactById = async (contactId) => {
   const contact = await contactsCollection.findById(contactId);
   return contact;
 };
+
+export const createContact = async (payload) => {
+  const contact = await contactsCollection.create(payload);
+  return contact;
+};
+
+// Функція для методу PATCH + PUT //
+
+export const updateContact = async (contactId, payload, options = {}) => {
+  const rawResult = await contactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    contact: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
+};
+
+// Функція лише для методу PATCH //
+
+// export const patchContact = async (contactId, payload) => {
+//   const rawResult = await contactsCollection.findOneAndUpdate(
+//     { _id: contactId },
+//     payload,
+//     {
+//       new: true,
+//     },
+//   );
+
+//   if (!rawResult) return null;
+
+//   return rawResult;
+// };
+
+export const deleteContact = async (contactId) => {
+  const contact = await contactsCollection.findOneAndDelete({ _id: contactId });
+  return contact;
+};
