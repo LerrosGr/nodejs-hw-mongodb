8b509cfb24b730b06b19b5d1b66d2ef157d7c87c
Lerros <108396158+LerrosGr@users.noreply.github.com>
2025-10-24 23:51:29 +0300

    swagger

--- src/server.js
+++ src/server.js
@@ -13,6 +13,8 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 import { UPLOAD_DIR } from './constants/index.js';
 
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
+
 dotenv.config();
 
 const PORT = Number(getEnvVar('PORT', '3000'));
@@ -31,6 +33,7 @@ export const setupServer = () => {
 
   app.use(router);
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use(notFoundHandler);
 
